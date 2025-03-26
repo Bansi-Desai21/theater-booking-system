5cea1b9d1fc71173098139016330654d1f938a09
Bansi-Desai21 <bansi.albiorix@gmail.com>
2025-03-26 11:42:29 +0530

    set up cron into github using yml file

--- src/cron-job/cronJob.controller.ts
+++ src/cron-job/cronJob.controller.ts
@@ -1,21 +1,31 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Headers, ForbiddenException } from "@nestjs/common";
 import { CronJobService } from "./cronJob.service";
 
 @Controller("cron")
 export class CronJobController {
+  private readonly API_KEY = process.env.CRON_API_KEY;
+
   constructor(private readonly cronJobService: CronJobService) {}
 
+  private validateApiKey(headers: any) {
+    if (headers["authorization"] !== `Bearer ${this.API_KEY}`) {
+      throw new ForbiddenException("Invalid API Key");
+    }
+  }
+
   @Get("sync")
-  async runSync() {
-    console.log("Triggered sync by cron-job.org");
+  async runSync(@Headers() headers) {
+    this.validateApiKey(headers);
+    console.log("Triggered sync by GitHub Actions");
     await this.cronJobService.syncMovies();
     return { message: "Sync triggered successfully!" };
   }
 
   @Get("delete-movies")
-  async deleteOldMovies() {
-    console.log("Triggered delete-movies by cron-job.org");
+  async deleteOldMovies(@Headers() headers) {
+    this.validateApiKey(headers);
+    console.log("Triggered delete-movies by GitHub Actions");
     await this.cronJobService.deleteOldMovies();
-    return { message: "Sync triggered successfully!" };
+    return { message: "Movie deletion triggered successfully!" };
   }
 }
